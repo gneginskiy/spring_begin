ae2c04e8f95a5bf88bca27e2d7ca5947b9b94955
Greg <Greg>
2017-04-30 19:19:49 +0300

    lesson 38: saving data in multiple tables, dividing Author into a separate entity

--- spring_db/src/main/java/dao/impls/SqliteMp3Dao.java
+++ spring_db/src/main/java/dao/impls/SqliteMp3Dao.java
@@ -9,6 +9,7 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.ResultSetExtractor;
 import org.springframework.jdbc.core.namedparam.SqlParameterSourceUtils;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
+import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Component;
 
 import javax.sql.DataSource;
@@ -24,37 +25,54 @@ public class SqliteMp3Dao implements MP3Dao {
     private JdbcTemplate jdbcTemplate;
     private SimpleJdbcInsert mp3SimpleJdbcInsert;
     private DataSource dataSource;
+    private SimpleJdbcInsert authorSimpleJdbcInsert;
 
     @Autowired
     public void setDataSource(DataSource dataSource) {
         this.jdbcTemplate = new JdbcTemplate(dataSource);
         this.dataSource = dataSource;
         this.mp3SimpleJdbcInsert = new SimpleJdbcInsert(dataSource).withTableName("MP3")
-                .usingColumns("NAME", "AUTHOR");
+                                                                   .usingColumns("NAME", "AUTHOR_ID")
+                                                                   .usingGeneratedKeyColumns("ID");
+        this.authorSimpleJdbcInsert = new SimpleJdbcInsert(dataSource).withTableName("AUTHOR")
+                                                                      .usingColumns("NAME")
+                                                                      .usingGeneratedKeyColumns("ID");
     }
+
 
     @Override
     public void insert(Mp3 mp3) {
-        mp3SimpleJdbcInsert.execute(new HashMap<String, String>() {{
-            put("name", mp3.getName());
-            put("author", mp3.getAuthor());
+        //assuming that the author doesn't exist already
+        KeyHolder authorKeyHolder = authorSimpleJdbcInsert.executeAndReturnKeyHolder(new HashMap<String, Object>() {{
+            put("name", mp3.getAuthor());
+            put("author_id", mp3.getAuthor());
         }});
-    }
 
-    public void insertWithQuery(Mp3 mp3) {
-        String sqlQuery = "INSERT INTO mp3 (name, author) VALUES (?,?)";
-        jdbcTemplate.update(sqlQuery, mp3.getName(), mp3.getAuthor());
-    }
-
-
-    public void batchInsert(Collection<Mp3> mp3Collection) {
-        mp3SimpleJdbcInsert.executeBatch(SqlParameterSourceUtils.createBatch(mp3Collection.toArray()));
+        mp3SimpleJdbcInsert.execute(new HashMap<String, Object>() {{
+            put("name", mp3.getName());
+            put("author_id", authorKeyHolder.getKey());
+        }});
     }
 
     public void insert(Collection<Mp3> mp3Collection) {
         mp3Collection.forEach(this::insert);
     }
 
+    //---------------------------------------The code below isn't so good-----------------------------------------------
+
+
+    @Deprecated
+    public void batchInsert(Collection<Mp3> mp3Collection) {
+        mp3Collection.forEach(this::insert);
+    }
+
+    @Deprecated
+    public void insertWithQuery(Mp3 mp3) {
+        String sqlQuery = "INSERT INTO mp3 (name, author) VALUES (?,?)";
+        jdbcTemplate.update(sqlQuery, mp3.getName(), mp3.getAuthor());
+    }
+
+    @Deprecated
     public void insertWithJdbc(Mp3 mp3) {
         Connection conn = null;
 
@@ -87,7 +105,7 @@ public class SqliteMp3Dao implements MP3Dao {
         }
     }
 
-
+    @Deprecated
     @Override
     public void update(Collection<Mp3> mp3Collection) {
         String sqlQuery = "UPDATE mp3 SET name = ?, author = ?  WHERE id = ?";
@@ -95,6 +113,7 @@ public class SqliteMp3Dao implements MP3Dao {
         jdbcTemplate.batchUpdate(sqlQuery, pss);
     }
 
+    @Deprecated
     @Override
     public void update(Mp3 mp3) {
         jdbcTemplate.update(
@@ -102,33 +121,38 @@ public class SqliteMp3Dao implements MP3Dao {
                 mp3.getName(), mp3.getAuthor(), mp3.getId());
     }
 
+    @Deprecated
     public void delete(Mp3 mp3) {
         String sqlQuery = "DELETE FROM mp3 WHERE name=? AND author=?";
         jdbcTemplate.update(sqlQuery, mp3.getName(), mp3.getAuthor());
     }
 
-
+    @Deprecated
     public Mp3 getMp3ById(int id) {
         String sqlQuery = "SELECT * FROM MP3 WHERE id=?";
         return jdbcTemplate.queryForObject(sqlQuery, new Mp3RowMapper(), id);
     }
 
+    @Deprecated
     public List<Mp3> getMp3ByName(String name) {
         String sqlQuery = "SELECT * FROM MP3 WHERE name=:name";
         return jdbcTemplate.query(sqlQuery, new Mp3RowMapper(), name);
     }
 
+    @Deprecated
     public List<Mp3> getMp3ByAuthor(String author) {
         String sqlQuery = "SELECT * FROM MP3 WHERE author=:author";
         return jdbcTemplate.query(sqlQuery, new Mp3RowMapper(), author);
     }
 
+    @Deprecated
     @Override
     public int getMp3Count() {
         String sqlQuery = "SELECT count(*) FROM MP3";
         return jdbcTemplate.queryForObject(sqlQuery, Integer.class);
     }
 
+    @Deprecated
     @Override
     public Map<String, Integer> getMp3CountForAuthors() {
         String sqlQuery = "SELECT author, count(*) AS COUNT FROM MP3 GROUP BY author";
